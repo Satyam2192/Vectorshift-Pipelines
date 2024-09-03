083be6c2daa2b8147a4a1dc99076f2e4b8fd6fdc
satyam2192 <satyam21092@gmail.com>
2024-09-03 14:16:50 +0530

    first commit

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -1,7 +1,7 @@
 import { useStore } from './store';
 import axios from 'axios';
 
-const BACKEND_URL = 'http://127.0.0.1:8000'; // Use the IP address instead of localhost
+const BACKEND_URL = 'https://vectorshift-pipelines-api.onrender.com'; 
 
 export const SubmitButton = () => {
     const { nodes, edges } = useStore();
